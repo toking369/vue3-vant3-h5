631df5fb104de8217bacca65893bc68a3f112c45
ex_lanlj2 <ex_lanlj2@partner.midea.com>
2021-07-06 09:56:22 +0800

    本地配置代理

--- vue.config.js
+++ vue.config.js
@@ -5,14 +5,24 @@
  * @LastEditors: ''
  * @LastEditTime: 2021-03-20 14:03:59
  */
+const config = require('./src/config/config.js')
 module.exports = {
   lintOnSave: false,
   productionSourceMap:false,
   publicPath:'./',
   transpileDependencies:[],
   devServer: {
-    host: '0.0.0.0',
-    port: 8080,
+    host:"localhost",
+    proxy: {
+      "/app": {
+        target: config.webpackUrl, //代理请求地址
+        secure: true, 
+        changeOrigin: true, 
+        pathRewrite: {
+          "^/app": "/app", 
+        }
+      }
+    }
   },
   chainWebpack: (config) => {
     config.resolve.symlinks(true) // 修复热更新失效
