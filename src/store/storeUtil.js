069b809bf3c4ea2e0eb86e8dc8565f141e2338f0
lanlianjiu <674074365@qq.com>
2023-07-18 13:02:03 +0800

    优化VUEX辅助函数以及说明文档

--- src/store/storeUtil.js
+++ src/store/storeUtil.js
@@ -20,42 +20,55 @@ export const userMapper = (middleValue, middleState, isComputed = false) => {
 	return stateMaps;
 };
 
+// 模块处理
+export const handleModule = (parmas) => {
+	const storeKey = Object.keys(parmas)[0];
+	const { mapModule, moduleValue, mapValue } = parmas;
+	const isModule = typeof moduleValue === "string" && moduleValue.length;
+
+	const middleState = isModule
+		? createNamespacedHelpers(moduleValue)[storeKey]
+		: mapModule;
+	const middleValue = isModule ? mapValue : moduleValue;
+	return { middleValue, middleState };
+};
+
 // 使用vuex辅助函数，states处理
 export const storeState = (moduleValue, mapValue) => {
-	const isModule = typeof moduleValue === "string" && moduleValue.length;
-	const middleState = isModule
-		? createNamespacedHelpers(moduleValue).mapState
-		: mapState;
-	const middleValue = isModule ? mapValue : moduleValue;
+	const { middleValue, middleState } = handleModule({
+		mapState,
+		moduleValue,
+		mapValue,
+	});
 	return userMapper(middleValue, middleState);
 };
 
 // 使用vuex辅助函数，Getter处理
 export const storeGetter = (moduleValue, mapValue) => {
-	const isModule = typeof moduleValue === "string" && moduleValue.length;
-	const middleGetter = isModule
-		? createNamespacedHelpers(moduleValue).mapGetters
-		: mapGetters;
-	const middleValue = isModule ? mapValue : moduleValue;
-	return userMapper(middleValue, middleGetter);
+	const { middleValue, middleState } = handleModule({
+		mapGetters,
+		moduleValue,
+		mapValue,
+	});
+	return userMapper(middleValue, middleState);
 };
 
 // 使用vuex辅助函数，Mutations处理
 export const storeMutation = (moduleValue, mapValue) => {
-	const isModule = typeof moduleValue === "string" && moduleValue.length;
-	const middleMutation = isModule
-		? createNamespacedHelpers(moduleValue).mapMutations
-		: mapMutations;
-	const middleValue = isModule ? mapValue : moduleValue;
-	return userMapper(middleValue, middleMutation, true);
+	const { middleValue, middleState } = handleModule({
+		mapMutations,
+		moduleValue,
+		mapValue,
+	});
+	return userMapper(middleValue, middleState, true);
 };
 
 // 使用vuex辅助函数，Action处理
 export const storeAction = (moduleValue, mapValue) => {
-	const isModule = typeof moduleValue === "string" && moduleValue.length;
-	const middleAction = isModule
-		? createNamespacedHelpers(moduleValue).mapActions
-		: mapActions;
-	const middleValue = isModule ? mapValue : moduleValue;
-	return userMapper(middleValue, middleAction, true);
+	const { middleValue, middleState } = handleModule({
+		mapActions,
+		moduleValue,
+		mapValue,
+	});
+	return userMapper(middleValue, middleState, true);
 };
